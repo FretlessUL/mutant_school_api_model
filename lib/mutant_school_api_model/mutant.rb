30085c014341c51618b2dec0dcae3f19e7a62794
Dave Strus <dave@getfretless.com>
2016-04-19 14:36:50 -0400

    Create empty Mutant class.

---
+++ lib/mutant_school_api_model/mutant.rb
@@ -0,0 +1,24 @@
+module MutantSchoolAPIModel
+  class Mutant
+    # Get all the mutants from the backend
+    # mutant = Mutant.all
+    def self.all
+    end
+
+    # Get a single mutant from the backend
+    # mutant = Mutant.find(3)
+    def self.find(id)
+    end
+
+    # Create or update a mutant in the backend
+    # mutant.save
+    def save
+    end
+
+    # Delete a mutant from the backend
+    # mutant.destroy
+    def destroy
+      #code
+    end
+  end
+end
