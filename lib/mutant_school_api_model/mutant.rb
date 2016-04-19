7aad3e39cdbdabc4c6ff369f2421af41cebd208b
Dave Strus <dave@getfretless.com>
2016-04-19 15:02:08 -0400

    Return instances of Mutant, rather than Hashes.

--- lib/mutant_school_api_model/mutant.rb
+++ lib/mutant_school_api_model/mutant.rb
@@ -5,18 +5,47 @@ module MutantSchoolAPIModel
       "https://mutant-school.herokuapp.com/api/v1/mutants"
     end
 
+    def self.attribute_names
+      [
+        :id,
+        :mutant_name,
+        :real_name,
+        :power,
+        :eligibility_begins_at,
+        :eligibility_ends_at,
+        :may_advise_beginning_at,
+        :url,
+        :created_at,
+        :updated_at,
+        :advisor
+      ]
+    end
+
     # Get all the mutants from the backend
     # mutant = Mutant.all
     def self.all
       response = HTTP.get(base_url)
-      JSON.parse(response.to_s)
+      JSON.parse(response.to_s).map do |attributes_hash|
+        Mutant.new(attributes_hash)
+      end
     end
 
     # Get a single mutant from the backend
     # mutant = Mutant.find(3)
     def self.find(id)
       response = HTTP.get("#{base_url}/#{id}")
-      JSON.parse(response.to_s)
+      Mutant.new(JSON.parse(response.to_s))
+    end
+
+    attr_accessor *attribute_names
+
+    def initialize(attr = {})
+      # Set instance variables from the items in the hash
+      attr.each do |name, value|
+        if self.class.attribute_names.include? name.to_sym
+          instance_variable_set("@#{name}", value)
+        end
+      end
     end
 
     # Create or update a mutant in the backend
